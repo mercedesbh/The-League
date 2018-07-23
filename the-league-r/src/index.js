7be2914dd19d681524123c3b9858a2c8fed2b73a
Mercedes Hall <mercedes@brandeis.edu>
2018-07-23 18:57:40 -0400

    adds redux setup

--- the-league-r/src/index.js
+++ the-league-r/src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
 // import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+//CreateStore creates redux store function, which is the main purpose of using redux
+import { createStore, applyMiddleware } from "redux";
+//Conects redux and react
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import rootReducer from "./rootReducer";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
-  document.getElementById('root'));
+  document.getElementById("root")
+);
 registerServiceWorker();
---
+++ the-league-r/src/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+
+//Reducer is a function that takes state and returns new state
+export default combineReducers({
+  user: () => ({})
+});
